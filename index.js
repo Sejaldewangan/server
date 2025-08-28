56761bfde52ff53d7e5c657f855f8c6379b803dc
sejaldewangan <shubham.dewangan.3440@gmail.com>
2025-08-28 18:30:24 +0530

    i2

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ import { data, students } from "./data.js";
 import mongoose from "mongoose";
 import router from "./routes/userRoute.js"
 import productRouter from "./routes/productRoute.js";
+// import { adminR } from "./routes/adminRoutes.js";
 const app = express();
 const port = 6969;
 app.use(express.json());
 app.use(cors());
-app.use("/demo", (req, res) => {
+app.get("/demo", (req, res) => {
   res.send("hi it is working");
 });
-app.use("/api/data/students",(req,res)=>{
+app.get("/api/data/students",(req,res)=>{
   res.json(students)
 })
 app.use("/api/create",router)
 app.use("/product",productRouter)
+// app.use("/api/v1",adminR)
 mongoose.connect('mongodb://localhost:27017/cruddb').then(
 console.log("mongoDB connected"),
   app.listen(port, () => {
---
+++ routes/adminRoutes.js
@@ -0,0 +1,6 @@
+// import { Router } from "express";
+// import { authenticateToken, authorizeRole } from "../middlewares/authMiddleware.js";
+
+// export const adminR= Router()
+
+// adminR.get("/admin", authenticateToken, authorizeRole("admin"), getAdmin);
